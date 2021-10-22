c6890cba5a1170cd83db53f558df32d8a7b77769
Mohamed Sobh <eng.m_sobhe@yahoo.com>
2021-10-22 12:47:28 +0100

    Add some styles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,13 @@
 import { Link } from "react-router-dom";
-import landingImg from "../resources/landingImg.jpg";
+
 
 const Home = () => {
     return (
-        <section>
-            <img src ={landingImg} alt= '' width='100%' height = '100% '/>
             <section>
                 <Link to = '/articles'>
                     <button>Articles</button>
                 </Link>
             </section>
-        </section>
-
     )
 };
 
